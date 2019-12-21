c29c48553edc792fbe2219817cb71a36459e738a
Artem Sapegin <artem.sapegin@omio.com>
2019-12-21 10:06:44 +0100

    Add initial tests

--- test/setup.js
+++ test/setup.js
@@ -5,4 +5,10 @@ global._ = require('lodash');
 global.React = require('react');
 
 // Doesn't work without this for some reason
+global.jest = jest;
 global.URLSearchParams = URLSearchParams;
+
+// Common components
+global.Button = (...props) =>
+  global.React.createElement('button', props);
+global.Text = (...props) => global.React.createElement('p', props);
